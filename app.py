cd1b25deddc55f241442969e46d50f178d1d0570
nazimsiddiqui1978 <nazim31s@gmail.com>
2025-12-14 04:45:47 -0500

    Updated JSON API data

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ def hello_world():
 
 if __name__ == "__main__":
 <<<<<<< HEAD
+<<<<<<< HEAD
     print(hello_world() + " from Mohd Nazim siddiqui")
 =======
     print(hello_world() + " from Nazim")
 >>>>>>> 6848780 (Added Flask project files)
+=======
+    print(hello_world() + " from Nazim siddiqui")
+>>>>>>> f2c71d3 (Updated JSON API data)
 def hello_world():
     return "hello, world"
