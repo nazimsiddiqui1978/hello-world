bf3011a3f2370975e957250a202a50e6d192e18a
nazimsiddiqui1978 <nazim31s@gmail.com>
2025-12-14 04:48:22 -0500

    Added To-Do form with item name and description

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ def hello_world():
 if __name__ == "__main__":
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     print(hello_world() + " from Mohd Nazim siddiqui")
 =======
     print(hello_world() + " from Nazim")
@@ -11,5 +12,8 @@ if __name__ == "__main__":
 =======
     print(hello_world() + " from Nazim siddiqui")
 >>>>>>> f2c71d3 (Updated JSON API data)
+=======
+    print(hello_world() + " from Mohd Nazim siddiqui")
+>>>>>>> 31e8229 (Added To-Do form with item name and description)
 def hello_world():
     return "hello, world"
