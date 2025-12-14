4d09fd554fa0b53d8240726e690cd50502c04e54
nazimsiddiqui1978 <nazim31s@gmail.com>
2025-12-14 04:43:21 -0500

    Added Flask project files

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ def hello_world():
     return "hello, world"
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     print(hello_world() + " from Mohd Nazim siddiqui")
+=======
+    print(hello_world() + " from Nazim")
+>>>>>>> 6848780 (Added Flask project files)
 def hello_world():
     return "hello, world"
